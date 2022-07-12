327e9b4a3481a30f09cfa99228d4611ddd9c02b0
Joblyn <oaikhenahjob@gmail.com>
2022-07-12 01:22:44 +0100

    change chromium_bin executable path for puppeteer

--- src/drivers/npm/driver.js
+++ src/drivers/npm/driver.js
@@ -14,11 +14,7 @@ const Wappalyzer = require('./wappalyzer')
 const { setTechnologies, setCategories, analyze, analyzeManyToMany, resolve } =
   Wappalyzer
 
-const {
-  //  CHROMIUM_BIN,
-  CHROMIUM_DATA_DIR,
-  CHROMIUM_WEBSOCKET,
-} = process.env
+const { CHROMIUM_BIN, CHROMIUM_DATA_DIR, CHROMIUM_WEBSOCKET } = process.env
 
 const chromiumArgs = [
   '--no-sandbox',
@@ -329,7 +325,7 @@ class Driver {
           ignoreHTTPSErrors: true,
           acceptInsecureCerts: true,
           args: chromiumArgs,
-          // executablePath: CHROMIUM_BIN,
+          executablePath: CHROMIUM_BIN,
         })
       }
 
